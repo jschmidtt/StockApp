79a7cebd4b84d4996b0e8a17a229d91ddc478841
justinschmidt <justinschmidttt@gmail.com>
2018-11-14 09:40:44 -0500

    Initial commit

---
+++ app/src/main/java/edu/temple/stockapp/MainActivity.java
@@ -0,0 +1,26 @@
+package edu.temple.stockapp;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton floatingActionButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        floatingActionButton = findViewById(R.id.floatingActionButton);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+    }
+}
